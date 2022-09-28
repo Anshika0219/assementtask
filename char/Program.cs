d44e4b094b5a633f1e4589850c0e5af0c76c1d2b
Anshika0219 <anshikaa@moback.com>
2022-09-28 10:57:55 +0530

    first commit

---
+++ char/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a string");
+            string str = Console.ReadLine();
+            Console.WriteLine("enter a charcter");
+            char value = Convert.ToChar(Console.ReadLine());
+            Boolean result = str.Contains(value);
+            Console.WriteLine(result);
+        }
+        }
+    }
+
---
+++ prime number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //...........prime number..............
+           Console.WriteLine("enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+             int i,count=0;
+             for (i = 2; i < num; i++)
+             {
+                 if (num % i == 0)
+                 {
+                     count++;
+                     break;
+                 }
+             }
+             if (count == 0)
+             {
+                 Console.WriteLine("Prime Number");
+             }
+             else
+             {
+                 Console.WriteLine("not a Prime Number");
+             }
+        }
+    }
+}
